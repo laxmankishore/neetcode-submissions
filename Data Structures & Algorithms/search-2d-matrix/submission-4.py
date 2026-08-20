43a913c372708aaf5d1ada1e585d4cc4106a3bd9
laxmankishore17 <koppisettilaxman21@gmail.com>
2026-08-19 18:51:04 -0700

    Add: search-2d-matrix - submission-4

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-4.py
@@ -0,0 +1,19 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        ROWS, COLS = len(matrix), len(matrix[0])
+
+        ## trying to solve in a bin search way
+        left, right = 0, ROWS * COLS - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            mid_row = mid // COLS
+            mid_col = mid % COLS
+            mid_num = matrix[mid_row][mid_col]
+
+            if mid_num == target : return True
+            if mid_num > target: right = mid - 1
+            else:
+                left = mid + 1
+            
+        return False
